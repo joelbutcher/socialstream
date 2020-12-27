8c8ceab231456185259ab777db5cb1a9011f78a9
Joel Butcher <joel@joelbutcher.co.uk>
2020-12-27 08:34:32 +0000

    fix connected accounts erros (inertia)

--- stubs/inertia/resources/js/Socialstream/ConnectedAccount.vue
+++ stubs/inertia/resources/js/Socialstream/ConnectedAccount.vue
@@ -1,31 +1,33 @@
 <template>
-    <div class="px-3 flex items-center justify-between">
-        <div class="flex items-center">
+    <div>
+        <div class="px-3 flex items-center justify-between">
+            <div class="flex items-center">
 
-            <facebook-icon class="h-6 w-6 mr-2" v-if="provider === 'facebook'"/>
-            <google-icon class="h-6 w-6 mr-2" v-if="provider === 'google'"/>
-            <twitter-icon class="h-6 w-6 mr-2" v-if="provider === 'twitter'"/>
-            <linked-in-icon class="h-6 w-6 mr-2" v-if="provider === 'linkedin'"/>
-            <github-icon class="h-6 w-6 mr-2" v-if="provider === 'github'"/>
-            <git-lab-icon class="h-6 w-6 mr-2" v-if="provider === 'gitlab'"/>
-            <bitbucket-icon class="h-6 w-6 mr-2" v-if="provider === 'bitbucket'"/>
+                <facebook-icon class="h-6 w-6 mr-2" v-if="provider === 'facebook'"/>
+                <google-icon class="h-6 w-6 mr-2" v-if="provider === 'google'"/>
+                <twitter-icon class="h-6 w-6 mr-2" v-if="provider === 'twitter'"/>
+                <linked-in-icon class="h-6 w-6 mr-2" v-if="provider === 'linkedin'"/>
+                <github-icon class="h-6 w-6 mr-2" v-if="provider === 'github'"/>
+                <git-lab-icon class="h-6 w-6 mr-2" v-if="provider === 'gitlab'"/>
+                <bitbucket-icon class="h-6 w-6 mr-2" v-if="provider === 'bitbucket'"/>
 
-            <div>
-                <div class="text-sm font-semibold text-gray-600">
-                    {{ provider.charAt(0).toUpperCase() + provider.slice(1)  }}
-                </div>
+                <div>
+                    <div class="text-sm font-semibold text-gray-600">
+                        {{ provider.charAt(0).toUpperCase() + provider.slice(1)  }}
+                    </div>
 
-                <div v-if="createdAt !== null" class="text-xs text-gray-500">
-                    Added on {{ createdAt }}
-                </div>
+                    <div v-if="createdAt !== null" class="text-xs text-gray-500">
+                        Added on {{ createdAt }}
+                    </div>
 
-                <div v-else class="text-xs text-gray-500">
-                    Not connected.
+                    <div v-else class="text-xs text-gray-500">
+                        Not connected.
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <slot name="action"></slot>
+            <slot name="action"></slot>
+        </div>
     </div>
 </template>
 
