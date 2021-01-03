1f22a8f89341b7d90ad7f5c21ae2fa22ae82b7f3
Joel Butcher <joel@joelbutcher.co.uk>
2021-01-03 16:59:46 +0000

    addded action and contract

---
+++ src/Contracts/GeneratesProviderRedirect.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace JoelButcher\Socialstream\Contracts;
+
+interface GeneratesProviderRedirect
+{
+    /**
+     * Generates the redirect for a given provider.
+     * 
+     * @param  string  $provider
+     * 
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function generate(string $provider);
+}
