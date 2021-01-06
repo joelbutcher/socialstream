8b9760c2acf0caf4cf50000007eb154e25c6eec9
Joel Butcher <joel@joelbutcher.co.uk>
2021-01-06 14:58:26 +0000

    fix set password form

--- stubs/inertia/resources/js/Pages/Profile/SetPasswordForm.vue
+++ stubs/inertia/resources/js/Pages/Profile/SetPasswordForm.vue
@@ -11,14 +11,14 @@
         <template #form>
             <div class="col-span-6 sm:col-span-4">
                 <jet-label for="password" value="New Password" />
-                <jet-input id="password" type="password" class="mt-1 block w-full" v-model="form.password" autocomplete="new-password" />
-                <jet-input-error :message="form.error('password')" class="mt-2" />
+                <jet-input id="password" type="password" class="mt-1 block w-full" v-model="form.password" ref="password" autocomplete="new-password" />
+                <jet-input-error :message="form.errors.password" class="mt-2" />
             </div>
 
             <div class="col-span-6 sm:col-span-4">
                 <jet-label for="password_confirmation" value="Confirm Password" />
                 <jet-input id="password_confirmation" type="password" class="mt-1 block w-full" v-model="form.password_confirmation" autocomplete="new-password" />
-                <jet-input-error :message="form.error('password_confirmation')" class="mt-2" />
+                <jet-input-error :message="form.errors.password_confirmation" class="mt-2" />
             </div>
         </template>
 
@@ -57,8 +57,6 @@
                 form: this.$inertia.form({
                     password: '',
                     password_confirmation: '',
-                }, {
-                    bag: 'setPassword',
                 }),
             }
         },
@@ -66,7 +64,15 @@
         methods: {
             setPassword() {
                 this.form.put(route('user-password.set'), {
+                    errorBag: 'setPassword',
                     preserveScroll: true,
+                    onSuccess: () => this.form.reset(),
+                    onError: () => {
+                        if (this.form.errors.password) {
+                            this.form.reset('password', 'password_confirmation')
+                            this.$refs.password.focus()
+                        }
+                    }
                 })
             },
         },
