65e8aa7d5969aacd6493eeb77e2360c6513698a4
Dante <35383529+srdante@users.noreply.github.com>
2021-01-12 17:51:15 -0300

    Use Fortify Action PasswordValidationRules

--- stubs/app/Actions/Socialstream/SetUserPassword.php
+++ stubs/app/Actions/Socialstream/SetUserPassword.php
@@ -2,13 +2,15 @@
 
 namespace App\Actions\Socialstream;
 
+use App\Actions\Fortify\PasswordValidationRules;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use JoelButcher\Socialstream\Contracts\SetsUserPasswords;
-use Laravel\Fortify\Rules\Password;
 
 class SetUserPassword implements SetsUserPasswords
 {
+    use PasswordValidationRules;
+
     /**
      * Validate and update the user's password.
      *
@@ -19,7 +21,7 @@ class SetUserPassword implements SetsUserPasswords
     public function set($user, array $input)
     {
         Validator::make($input, [
-            'password' => ['required', 'string', new Password, 'confirmed'],
+            'password' => $this->passwordRules(),
         ])->validateWithBag('setPassword');
 
         $user->forceFill([
