d5a182ae29075ff24dde00c4f81e5f4d0b940ea2
Joel Butcher <joel@joelbutcher.co.uk>
2021-01-29 16:34:44 +0000

    wip

---
+++ src/Features.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace JoelButcher\Socialstream;
+
+class Features
+{
+    /**
+     * Determine if the given feature is enabled.
+     *
+     * @param  string  $feature
+     * @return bool
+     */
+    public static function enabled(string $feature)
+    {
+        return in_array($feature, config('socialstream.features', []));
+    }
+
+    /**
+     * Determine if the application supports creating accounts
+     * when logging in for the first time via a provider.
+     * 
+     * @return bool
+     */
+    public static function createsAccountsOnFirstLogin()
+    {
+        return static::enabled(static::createAccountOnFirstLogin());
+    }
+
+    /**
+     * Enable the create account on first login feature.
+     *
+     * @return string
+     */
+    public static function createAccountOnFirstLogin()
+    {
+        return 'create-account-on-first-login';
+    }
+}
