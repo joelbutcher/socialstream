ea7382bfc16b259e4bac7e5282157b509d6b7cf4
Joel Butcher <joelbutcher@users.noreply.github.com>
2021-02-04 18:39:35 +0000

    Apply fixes from StyleCI

--- stubs/app/Actions/Socialstream/CreateUserFromProvider.php
+++ stubs/app/Actions/Socialstream/CreateUserFromProvider.php
@@ -48,7 +48,7 @@ class CreateUserFromProvider implements CreatesUserFromProvider
 
                 if (Socialstream::hasProviderAvatarsFeature() && Jetstream::managesProfilePhotos() && $providerUser->getAvatar()) {
                     $name = pathinfo($providerUser->getAvatar())['basename'];
-                    file_put_contents($file = '/tmp/' . $name, file_get_contents($providerUser->getAvatar()));
+                    file_put_contents($file = '/tmp/'.$name, file_get_contents($providerUser->getAvatar()));
                     $user->updateProfilePhoto(new UploadedFile($file, $name));
                 }
 
