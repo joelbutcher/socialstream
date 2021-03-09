fc56e1df18a4584764a5cce543795ae34ab55673
Joel Butcher <joel@joelbutcher.co.uk>
2021-03-09 13:54:01 +0000

    hotfix

--- src/SocialstreamServiceProvider.php
+++ src/SocialstreamServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\View\Compilers\BladeCompiler;
+use JoelButcher\Socialstream\Actions\ResolveSocialiteUser;
 use JoelButcher\Socialstream\Http\Livewire\ConnectedAccountsForm;
 use JoelButcher\Socialstream\Http\Livewire\SetPasswordForm;
 use JoelButcher\Socialstream\Http\Middleware\ShareInertiaData;
