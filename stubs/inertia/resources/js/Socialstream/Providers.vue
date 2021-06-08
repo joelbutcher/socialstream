38fbed3e50e5d45a6d5b4cee8d1f6b2ea7a27d56
srdante <minidantebtc@gmail.com>
2021-06-08 17:02:04 -0300

    Accessibility improvement, inertia

--- stubs/inertia/resources/js/Socialstream/Providers.vue
+++ stubs/inertia/resources/js/Socialstream/Providers.vue
@@ -9,30 +9,37 @@
         <div class="flex items-center justify-center">
             <a v-if="$page.props.socialstream.providers.includes('facebook')" :href="route('oauth.redirect', 'facebook')">
                 <facebook-icon class="h-6 w-6 mx-2" />
+                <span class="sr-only">Facebook</span>
             </a>
 
             <a v-if="$page.props.socialstream.providers.includes('google')" :href="route('oauth.redirect', 'google')" >
                 <google-icon class="h-6 w-6 mx-2" />
+                <span class="sr-only">Google</span>
             </a>
 
             <a v-if="$page.props.socialstream.providers.includes('twitter')" :href="route('oauth.redirect', 'twitter')">
                 <twitter-icon class="h-6 w-6 mx-2" />
+                <span class="sr-only">Twitter</span>
             </a>
 
             <a v-if="$page.props.socialstream.providers.includes('linkedin')" :href="route('oauth.redirect', 'linkedin')">
                 <linked-in-icon class="h-6 w-6 mx-2" />
+                <span class="sr-only">LinkedIn</span>
             </a>
 
             <a v-if="$page.props.socialstream.providers.includes('github')" :href="route('oauth.redirect', 'github')">
                 <github-icon class="h-6 w-6 mx-2" />
+                <span class="sr-only">Github</span>
             </a>
 
             <a v-if="$page.props.socialstream.providers.includes('gitlab')" :href="route('oauth.redirect', 'gitlab')">
                 <git-lab-icon class="h-6 w-6 mx-2" />
+                <span class="sr-only">GitLab</span>
             </a>
 
             <a v-if="$page.props.socialstream.providers.includes('bitbucket')" :href="route('oauth.redirect', 'bitbucket')">
                 <bitbucket-icon class="h-6 w-6 mx-2" />
+                <span class="sr-only">BitBucket</span>
             </a>
         </div>
     </div>
