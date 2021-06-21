9e790064cfb4bf08d6b94c0218ca05ceee084c3a
sesha <schandrappa@javelinagency.com>
2021-06-21 10:12:45 +0530

    fixed HandlesInvalidState issue

--- src/Http/Controllers/OAuthController.php
+++ src/Http/Controllers/OAuthController.php
@@ -2,7 +2,7 @@
 
 namespace JoelButcher\Socialstream\Http\Controllers;
 
-use App\Actions\Socialstream\HandleInvalidState;
+use JoelButcher\Socialstream\Contracts\HandlesInvalidState;
 use Illuminate\Contracts\Auth\StatefulGuard;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
@@ -67,7 +67,7 @@ class OAuthController extends Controller
         CreatesUserFromProvider $createsUser,
         CreatesConnectedAccounts $createsConnectedAccounts,
         UpdatesConnectedAccounts $updatesConnectedAccounts,
-        HandleInvalidState $invalidStateHandler
+        HandlesInvalidState $invalidStateHandler
     ) {
         $this->guard = $guard;
         $this->createsUser = $createsUser;
