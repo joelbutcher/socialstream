4bfaff2939ea6711c68cbbd95611750632f18ed0
Ibnu <ibnu.magang@gmail.com>
2022-05-22 13:25:18 +0700

    remove unused imports

--- stubs/inertia/resources/js/Pages/Auth/Register.vue
+++ stubs/inertia/resources/js/Pages/Auth/Register.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { Head, Link, useForm, usePage } from '@inertiajs/inertia-vue3';
+import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
 import JetAuthenticationCard from '@/Jetstream/AuthenticationCard.vue';
 import JetAuthenticationCardLogo from '@/Jetstream/AuthenticationCardLogo.vue';
 import JetButton from '@/Jetstream/Button.vue';
