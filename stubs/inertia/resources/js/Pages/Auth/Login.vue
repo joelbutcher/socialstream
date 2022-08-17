e35f79e8bde03a59a4e1dd6597159c0e02528f5d
Joel Butcher <joelbutcher@users.noreply.github.com>
2022-08-17 21:49:41 +0100

    [3.x] Fix validation errors and connect account form (#215)
    * Fix overriding changes to Jetstream's display of validation error
    
    * Display socialstream errors underneath the socialstream component
    
    * Update install command
    
    * Apply fixes from StyleCI
    
    * revert change to icons folder
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- stubs/inertia/resources/js/Pages/Auth/Login.vue
+++ stubs/inertia/resources/js/Pages/Auth/Login.vue
@@ -4,78 +4,90 @@ import JetAuthenticationCard from '@/Components/AuthenticationCard.vue';
 import JetAuthenticationCardLogo from '@/Components/AuthenticationCardLogo.vue';
 import JetButton from '@/Components/Button.vue';
 import JetInput from '@/Components/Input.vue';
+import JetInputError from '@/Components/InputError.vue';
 import JetCheckbox from '@/Components/Checkbox.vue';
 import JetLabel from '@/Components/Label.vue';
-import JetValidationErrors from '@/Components/ValidationErrors.vue';
-import SocialstreamProviders from '@/Components/SocialstreamProviders.vue';
+import SocialstreamProviders from '@/Components/Socialstream.vue';
 
 defineProps({
-    canResetPassword: Boolean,
-    status: String,
+  canResetPassword: Boolean,
+  status: String,
 });
 
 const form = useForm({
-    email: '',
-    password: '',
-    remember: false,
+  email: '',
+  password: '',
+  remember: false,
 });
 
 const submit = () => {
-    form.transform(data => ({
-        ...data,
-        remember: form.remember ? 'on' : '',
-    })).post(route('login'), {
-        onFinish: () => form.reset('password'),
-    });
+  form.transform(data => ({
+    ...data,
+    remember: form.remember ? 'on' : '',
+  })).post(route('login'), {
+    onFinish: () => form.reset('password'),
+  });
 };
 </script>
 
 <template>
-    <Head title="Log in" />
+  <Head title="Log in" />
 
-    <JetAuthenticationCard>
-        <template #logo>
-            <JetAuthenticationCardLogo />
-        </template>
+  <JetAuthenticationCard>
+    <template #logo>
+      <JetAuthenticationCardLogo />
+    </template>
 
-        <JetValidationErrors class="mb-4" />
+    <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+      {{ status }}
+    </div>
 
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
-        </div>
+    <form @submit.prevent="submit">
+      <div>
+        <JetLabel for="email" value="Email" />
+        <JetInput
+            id="email"
+            v-model="form.email"
+            type="email"
+            class="mt-1 block w-full"
+            required
+            autofocus
+        />
+        <JetInputError class="mt-2" :message="form.errors.email" />
+      </div>
 
-        <form @submit.prevent="submit">
-            <div>
-                <JetLabel for="email" value="Email" />
-                <JetInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus />
-            </div>
+      <div class="mt-4">
+        <JetLabel for="password" value="Password" />
+        <JetInput
+            id="password"
+            v-model="form.password"
+            type="password"
+            class="mt-1 block w-full"
+            required
+            autocomplete="current-password"
+        />
+        <JetInputError class="mt-2" :message="form.errors.password" />
+      </div>
 
-            <div class="mt-4">
-                <JetLabel for="password" value="Password" />
-                <JetInput id="password" type="password" class="mt-1 block w-full" v-model="form.password" required
-                    autocomplete="current-password" />
-            </div>
+      <div class="block mt-4">
+        <label class="flex items-center">
+          <JetCheckbox v-model:checked="form.remember" name="remember" />
+          <span class="ml-2 text-sm text-gray-600">Remember me</span>
+        </label>
+      </div>
 
-            <div class="block mt-4">
-                <label class="flex items-center">
-                    <JetCheckbox name="remember" v-model="form.remember" />
-                    <span class="ml-2 text-sm text-gray-600">Remember me</span>
-                </label>
-            </div>
+      <div class="flex items-center justify-end mt-4">
+        <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900">
+          Forgot your password?
+        </Link>
 
-            <div class="flex items-center justify-end mt-4">
-                <Link v-if="canResetPassword" :href="route('password.request')"
-                    class="underline text-sm text-gray-600 hover:text-gray-900">
-                Forgot your password?
-                </Link>
+        <JetButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+          Log in
+        </JetButton>
+      </div>
+    </form>
 
-                <JetButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Login
-                </JetButton>
-            </div>
-        </form>
+    <SocialstreamProviders v-if="$page.props.socialstream.show" />
 
-        <SocialstreamProviders v-if="$page.props.socialstream.show" />
-
-    </JetAuthenticationCard>
+  </JetAuthenticationCard>
 </template>
--- stubs/inertia/resources/js/Pages/Auth/Register.vue
+++ stubs/inertia/resources/js/Pages/Auth/Register.vue
@@ -4,86 +4,111 @@ import JetAuthenticationCard from '@/Components/AuthenticationCard.vue';
 import JetAuthenticationCardLogo from '@/Components/AuthenticationCardLogo.vue';
 import JetButton from '@/Components/Button.vue';
 import JetInput from '@/Components/Input.vue';
+import JetInputError from '@/Components/InputError.vue';
 import JetCheckbox from '@/Components/Checkbox.vue';
 import JetLabel from '@/Components/Label.vue';
-import JetValidationErrors from '@/Components/ValidationErrors.vue';
-import SocialstreamProviders from '@/Components/SocialstreamProviders.vue';
+import SocialstreamProviders from '@/Components/Socialstream.vue';
 
 const form = useForm({
-    name: '',
-    email: '',
-    password: '',
-    password_confirmation: '',
-    terms: false,
+  name: '',
+  email: '',
+  password: '',
+  password_confirmation: '',
+  terms: false,
 });
 
 const submit = () => {
-    form.post(route('register'), {
-        onFinish: () => form.reset('password', 'password_confirmation'),
-    });
+  form.post(route('register'), {
+    onFinish: () => form.reset('password', 'password_confirmation'),
+  });
 };
 </script>
 
 <template>
-    <Head title="Register" />
+  <Head title="Register" />
 
-    <JetAuthenticationCard>
-        <template #logo>
-            <JetAuthenticationCardLogo />
-        </template>
+  <JetAuthenticationCard>
+    <template #logo>
+      <JetAuthenticationCardLogo />
+    </template>
 
-        <JetValidationErrors class="mb-4" />
+    <form @submit.prevent="submit">
+      <div>
+        <JetLabel for="name" value="Name" />
+        <JetInput
+            id="name"
+            v-model="form.name"
+            type="text"
+            class="mt-1 block w-full"
+            required
+            autofocus
+            autocomplete="name"
+        />
+        <JetInputError class="mt-2" :message="form.errors.name" />
+      </div>
 
-        <form @submit.prevent="submit">
-            <div>
-                <JetLabel for="name" value="Name" />
-                <JetInput id="name" v-model="form.name" type="text" class="mt-1 block w-full" required autofocus
-                    autocomplete="name" />
+      <div class="mt-4">
+        <JetLabel for="email" value="Email" />
+        <JetInput
+            id="email"
+            v-model="form.email"
+            type="email"
+            class="mt-1 block w-full"
+            required
+        />
+        <JetInputError class="mt-2" :message="form.errors.email" />
+      </div>
+
+      <div class="mt-4">
+        <JetLabel for="password" value="Password" />
+        <JetInput
+            id="password"
+            v-model="form.password"
+            type="password"
+            class="mt-1 block w-full"
+            required
+            autocomplete="new-password"
+        />
+        <JetInputError class="mt-2" :message="form.errors.password" />
+      </div>
+
+      <div class="mt-4">
+        <JetLabel for="password_confirmation" value="Confirm Password" />
+        <JetInput
+            id="password_confirmation"
+            v-model="form.password_confirmation"
+            type="password"
+            class="mt-1 block w-full"
+            required
+            autocomplete="new-password"
+        />
+        <JetInputError class="mt-2" :message="form.errors.password_confirmation" />
+      </div>
+
+      <div v-if="$page.props.jetstream.hasTermsAndPrivacyPolicyFeature" class="mt-4">
+        <JetLabel for="terms">
+          <div class="flex items-center">
+            <JetCheckbox id="terms" v-model:checked="form.terms" name="terms" />
+
+            <div class="ml-2">
+              I agree to the <a target="_blank" :href="route('terms.show')" class="underline text-sm text-gray-600 hover:text-gray-900">Terms of Service</a> and <a target="_blank" :href="route('policy.show')" class="underline text-sm text-gray-600 hover:text-gray-900">Privacy Policy</a>
             </div>
+          </div>
+        </JetLabel>
+      </div>
 
-            <div class="mt-4">
-                <JetLabel for="email" value="Email" />
-                <JetInput id="email" v-model="form.email" type="email" class="mt-1 block w-full" required />
-            </div>
+      <div class="flex items-center justify-end mt-4">
+        <Link :href="route('login')" class="underline text-sm text-gray-600 hover:text-gray-900">
+          Already registered?
+        </Link>
 
-            <div class="mt-4">
-                <JetLabel for="password" value="Password" />
-                <JetInput id="password" v-model="form.password" type="password" class="mt-1 block w-full" required
-                    autocomplete="new-password" />
-            </div>
+        <JetButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+          Register
+        </JetButton>
+      </div>
+    </form>
 
-            <div class="mt-4">
-                <JetLabel for="password_confirmation" value="Confirm Password" />
-                <JetInput id="password_confirmation" v-model="form.password_confirmation" type="password"
-                    class="mt-1 block w-full" required autocomplete="new-password" />
-            </div>
+    <SocialstreamProviders v-if="$page.props.socialstream.show" />
 
-            <div v-if="$page.props.jetstream.hasTermsAndPrivacyPolicyFeature" class="mt-4">
-                <JetLabel for="terms">
-                    <div class="flex items-center">
-                        <JetCheckbox id="terms" v-model:checked="form.terms" name="terms" />
-
-                        <div class="ml-2">
-                            I agree to the <a target="_blank" :href="route('terms.show')"
-                                class="underline text-sm text-gray-600 hover:text-gray-900">Terms of Service</a> and <a
-                                target="_blank" :href="route('policy.show')"
-                                class="underline text-sm text-gray-600 hover:text-gray-900">Privacy Policy</a>
-                        </div>
-                    </div>
-                </JetLabel>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <Link :href="route('login')" class="underline text-sm text-gray-600 hover:text-gray-900">
-                Already registered?
-                </Link>
-
-                <JetButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Register
-                </JetButton>
-            </div>
-        </form>
-
-        <SocialstreamProviders v-if="$page.props.socialstream.show" />
-    </JetAuthenticationCard>
+  </JetAuthenticationCard>
 </template>
