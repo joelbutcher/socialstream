13501e6e707150fe944b99ce6b3de1cf5da44f76
Beej <howdu@users.noreply.github.com>
2022-09-15 21:26:00 +0100

    Replace file_get_contents with a Http client request (#222)

--- src/SetsProfilePhotoFromUrl.php
+++ src/SetsProfilePhotoFromUrl.php
@@ -4,6 +4,7 @@ namespace JoelButcher\Socialstream;
 
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Http;
 
 trait SetsProfilePhotoFromUrl
 {
@@ -16,7 +17,11 @@ trait SetsProfilePhotoFromUrl
     public function setProfilePhotoFromUrl(string $url)
     {
         $name = pathinfo($url)['basename'];
-        file_put_contents($file = sys_get_temp_dir().'/'.Str::uuid()->toString(), file_get_contents($url));
-        $this->updateProfilePhoto(new UploadedFile($file, $name));
+        $photo = Http::get($url);
+        if ($photo) {
+            file_put_contents($file = sys_get_temp_dir().'/'.Str::uuid()->toString(), $photo);
+
+            $this->updateProfilePhoto(new UploadedFile($file, $name));
+        }
     }
 }
