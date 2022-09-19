30553611bd61822f747c26dfd301bef2882d8b02
Salvatore Scalzi <scalzi.salvatore@gmail.com>
2022-09-19 19:38:45 +0200

    [3.x] Ensure HTTP response for fetching avatar is successful (#224)

--- src/SetsProfilePhotoFromUrl.php
+++ src/SetsProfilePhotoFromUrl.php
@@ -17,11 +17,16 @@ trait SetsProfilePhotoFromUrl
     public function setProfilePhotoFromUrl(string $url)
     {
         $name = pathinfo($url)['basename'];
-        $photo = Http::get($url);
-        if ($photo) {
-            file_put_contents($file = sys_get_temp_dir().'/'.Str::uuid()->toString(), $photo);
+        $response = Http::get($url);
+
+        //Determine if the status code is >= 200 and < 300
+        if ($response->successful()) {
+            file_put_contents($file = sys_get_temp_dir().'/'.Str::uuid()->toString(), $response);
 
             $this->updateProfilePhoto(new UploadedFile($file, $name));
+        } else {
+            session()->flash('flash.banner', 'Unable to retrive image');
+            session()->flash('flash.bannerStyle', 'danger');
         }
     }
 }
