6d072c1ef0a7ce415a238742c127d9e362f13d78
Joel Butcher <joel167897@gmail.com>
2023-02-15 15:39:37 +0000

    Fix share inertia data (again)

--- src/Http/Middleware/ShareInertiaData.php
+++ src/Http/Middleware/ShareInertiaData.php
@@ -3,6 +3,7 @@
 namespace JoelButcher\Socialstream\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -15,7 +16,7 @@ class ShareInertiaData
     /**
      * Handle the incoming request.
      */
-    public function handle(Request $request, Closure $next): Response|RedirectResponse
+    public function handle(Request $request, Closure $next): Response|RedirectResponse|JsonResponse
     {
         Inertia::share(array_filter([
             'socialstream' => function () use ($request) {
