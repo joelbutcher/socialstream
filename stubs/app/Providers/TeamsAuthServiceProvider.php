fd0f65d266383b446467be512fe9fe7e427d8de8
Joel Butcher <joel167897@gmail.com>
2023-02-15 15:30:22 +0000

    update missing type declarations

--- stubs/app/Providers/TeamsAuthServiceProvider.php
+++ stubs/app/Providers/TeamsAuthServiceProvider.php
@@ -13,7 +13,7 @@ class AuthServiceProvider extends ServiceProvider
     /**
      * The policy mappings for the application.
      *
-     * @var array
+     * @var array<class-string, class-string>
      */
     protected $policies = [
         Team::class => TeamPolicy::class,
@@ -22,10 +22,8 @@ class AuthServiceProvider extends ServiceProvider
 
     /**
      * Register any authentication / authorization services.
-     *
-     * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         $this->registerPolicies();
 
