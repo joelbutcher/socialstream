4d247dada345d40f6efc3453ac3ce533482984ce
Joel Butcher <joelbutcher@users.noreply.github.com>
2023-03-05 14:57:53 +0000

    Publish `GenerateRedirectForProvider` action when installing the inertia stack (#261)

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Command;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Str;
 use Laravel\Jetstream\Jetstream;
+use Symfony\Component\Process\Process;
 
 class InstallCommand extends Command
 {
@@ -14,7 +15,14 @@ class InstallCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'socialstream:install';
+    protected $signature = 'socialstream:install 
+                            {--stack= : Indicates the desired stack to be installed (Livewire, Inertia)}
+                            {--teams : Indicates if team support should be installed}
+                            {--api : Indicates if API support should be installed}
+                            {--verification : Indicates if email verification support should be installed}
+                            {--pest : Indicates if Pest should be installed}
+                            {--ssr : Indicates if Inertia SSR support should be installed}
+                            {--composer=global : Absolute path to the Composer binary which should be used to install packages}';
 
     /**
      * The console command description.
@@ -26,27 +34,33 @@ class InstallCommand extends Command
     /**
      * Execute the console command.
      *
-     * @return void
+     * @return int|null
      */
     public function handle()
     {
         // Check if Jetstream has been installed.
         if (! file_exists(config_path('jetstream.php'))) {
-            $this->warn('Jetstream hasn\'t been installed. This package requires Jetstream to be installed.');
+            $this->components->warn('Jetstream hasn\'t been installed. Installing now...');
 
-            if ($this->ask('Do you want to install Jetstream? (yes/no)', 'no') !== 'yes') {
-                return 0;
+            $stack = $this->option('stack') ?: $this->components->choice('Which stack would you like to use [inertia] or [livewire]?', ['inertia', 'livewire']);
+
+            if (! in_array($stack, ['inertia', 'livewire'])) {
+                $this->components->error('Invalid stack. Supported stacks are [inertia] and [livewire].');
+
+                return 1;
             }
 
-            $stack = $this->choice('Which Jetstream stack do you prefer', ['livewire', 'inertia']);
-
-            $useTeams = $this->ask('Will your application use teams? (yes/no)', 'no') === 'yes';
-
-            $this->callSilent('jetstream:install', ['stack' => $stack, '--teams' => $useTeams]);
+            $this->call('jetstream:install', [
+                'stack' => $stack,
+                '--teams' => $this->option('teams'),
+                '--api' => $this->option('api'),
+                '--verification' => $this->option('verification'),
+                '--pest' => $this->option('pest'),
+                '--ssr' => $this->option('ssr'),
+                '--composer' => $this->option('composer'),
+            ]);
         } else {
             $stack = config('jetstream.stack');
-
-            $useTeams = Jetstream::hasTeamFeatures();
         }
 
         // Publish...
@@ -59,13 +73,19 @@ class InstallCommand extends Command
             $this->installInertiaStack();
         }
 
-        if ($useTeams) {
+        if ($this->option('teams')) {
             $this->ensureTeamsCompatibility();
         }
 
+        // Tests...
+        $stubs = $this->getTestStubsPath();
+        copy($stubs.'/SocialstreamRegistrationTest.php', base_path('tests/Feature/SocialstreamRegistrationTest.php'));
+
         $this->line('');
-        $this->info('Socialstream installed successfully.');
-        $this->comment('Please execute "npm install && npm run dev" to build your assets.');
+        $this->components->info('Socialstream installed successfully.');
+        $this->components->info('Running [npm install && npm run build]...');
+
+        $this->installNodeDependenciesAndBuild();
 
         return 0;
     }
@@ -84,9 +104,8 @@ class InstallCommand extends Command
         (new Filesystem)->ensureDirectoryExists(resource_path('views/profile'));
         (new Filesystem)->ensureDirectoryExists(resource_path('views/components'));
 
-        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/resources/views/components', resource_path('views/components'));
-
         // Service Providers...
+        copy(__DIR__.'/../../stubs/app/Providers/AuthServiceProvider.php', app_path('Providers/AuthServiceProvider.php'));
         copy(__DIR__.'/../../stubs/app/Providers/SocialstreamServiceProvider.php', app_path('Providers/SocialstreamServiceProvider.php'));
         $this->installServiceProviderAfter('JetstreamServiceProvider', 'SocialstreamServiceProvider');
 
@@ -94,30 +113,36 @@ class InstallCommand extends Command
         copy(__DIR__.'/../../stubs/app/Models/User.php', app_path('Models/User.php'));
         copy(__DIR__.'/../../stubs/app/Models/ConnectedAccount.php', app_path('Models/ConnectedAccount.php'));
 
+        // Policies
+        (new Filesystem)->ensureDirectoryExists(app_path('Policies'));
+        copy(__DIR__.'/../../stubs/app/Policies/ConnectedAccountPolicy.php', app_path('Policies/ConnectedAccountPolicy.php'));
+
         // Jetstream Actions...
         copy(__DIR__.'/../../stubs/app/Actions/Jetstream/DeleteUser.php', app_path('Actions/Jetstream/DeleteUser.php'));
 
         // Actions...
+        copy(__DIR__.'/../../stubs/app/Actions/Socialstream/ResolveSocialiteUser.php', app_path('Actions/Socialstream/ResolveSocialiteUser.php'));
         copy(__DIR__.'/../../stubs/app/Actions/Socialstream/CreateConnectedAccount.php', app_path('Actions/Socialstream/CreateConnectedAccount.php'));
+        copy(__DIR__.'/../../stubs/app/Actions/Socialstream/GenerateRedirectForProvider.php', app_path('Actions/Socialstream/GenerateRedirectForProvider.php'));
+        copy(__DIR__.'/../../stubs/app/Actions/Socialstream/UpdateConnectedAccount.php', app_path('Actions/Socialstream/UpdateConnectedAccount.php'));
         copy(__DIR__.'/../../stubs/app/Actions/Socialstream/CreateUserFromProvider.php', app_path('Actions/Socialstream/CreateUserFromProvider.php'));
         copy(__DIR__.'/../../stubs/app/Actions/Socialstream/GenerateRedirectForProvider.php', app_path('Actions/Socialstream/GenerateRedirectForProvider.php'));
         copy(__DIR__.'/../../stubs/app/Actions/Socialstream/HandleInvalidState.php', app_path('Actions/Socialstream/HandleInvalidState.php'));
         copy(__DIR__.'/../../stubs/app/Actions/Socialstream/SetUserPassword.php', app_path('Actions/Socialstream/SetUserPassword.php'));
 
         // Auth views...
-        copy(__DIR__.'/../../stubs/resources/views/auth/login.blade.php', resource_path('views/auth/login.blade.php'));
-        copy(__DIR__.'/../../stubs/resources/views/auth/register.blade.php', resource_path('views/auth/register.blade.php'));
+        copy(__DIR__.'/../../stubs/livewire/resources/views/auth/login.blade.php', resource_path('views/auth/login.blade.php'));
+        copy(__DIR__.'/../../stubs/livewire/resources/views/auth/register.blade.php', resource_path('views/auth/register.blade.php'));
 
-        // Components...
-        copy(__DIR__.'/../../stubs/livewire/resources/views/components/action-link.blade.php', resource_path('views/components/action-link.blade.php'));
-        copy(__DIR__.'/../../stubs/livewire/resources/views/components/connected-account.blade.php', resource_path('views/components/connected-account.blade.php'));
+        // Custom components...
+        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/livewire/resources/views/components', resource_path('views/components'));
 
         // Profile views...
         copy(__DIR__.'/../../stubs/livewire/resources/views/profile/connected-accounts-form.blade.php', resource_path('views/profile/connected-accounts-form.blade.php'));
         copy(__DIR__.'/../../stubs/livewire/resources/views/profile/set-password-form.blade.php', resource_path('views/profile/set-password-form.blade.php'));
         copy(__DIR__.'/../../stubs/livewire/resources/views/profile/show.blade.php', resource_path('views/profile/show.blade.php'));
 
-        $this->replaceInFile("// 'github',", "'github'", config_path('socialstream.php'));
+        $this->replaceInFile('// Providers::github(),', 'Providers::github(),', config_path('socialstream.php'));
     }
 
     /**
@@ -131,28 +156,30 @@ class InstallCommand extends Command
         (new Filesystem)->ensureDirectoryExists(app_path('Actions/Jetstream'));
         (new Filesystem)->ensureDirectoryExists(app_path('Actions/Socialstream'));
         (new Filesystem)->ensureDirectoryExists(resource_path('js/Socialstream'));
-        (new Filesystem)->ensureDirectoryExists(resource_path('js/ProviderIcons'));
+        (new Filesystem)->ensureDirectoryExists(resource_path('js/Pages/Auth'));
         (new Filesystem)->ensureDirectoryExists(resource_path('js/Pages/Profile'));
-        (new Filesystem)->ensureDirectoryExists(resource_path('views/components'));
-
-        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/resources/views/components', resource_path('views/components'));
 
         // Service Providers...
+        copy(__DIR__.'/../../stubs/app/Providers/AuthServiceProvider.php', app_path('Providers/AuthServiceProvider.php'));
         copy(__DIR__.'/../../stubs/app/Providers/SocialstreamServiceProvider.php', app_path('Providers/SocialstreamServiceProvider.php'));
         $this->installServiceProviderAfter('JetstreamServiceProvider', 'SocialstreamServiceProvider');
-
-        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/inertia/resources/js/Socialstream', resource_path('js/Socialstream'));
-        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/inertia/resources/js/ProviderIcons', resource_path('js/ProviderIcons'));
 
         // Models...
         copy(__DIR__.'/../../stubs/app/Models/User.php', app_path('Models/User.php'));
         copy(__DIR__.'/../../stubs/app/Models/ConnectedAccount.php', app_path('Models/ConnectedAccount.php'));
 
+        // Policies
+        (new Filesystem)->ensureDirectoryExists(app_path('Policies'));
+        copy(__DIR__.'/../../stubs/app/Policies/ConnectedAccountPolicy.php', app_path('Policies/ConnectedAccountPolicy.php'));
+
         // Jetstream Actions...
         copy(__DIR__.'/../../stubs/app/Actions/Jetstream/DeleteUser.php', app_path('Actions/Jetstream/DeleteUser.php'));
 
         // Actions...
+        copy(__DIR__.'/../../stubs/app/Actions/Socialstream/ResolveSocialiteUser.php', app_path('Actions/Socialstream/ResolveSocialiteUser.php'));
         copy(__DIR__.'/../../stubs/app/Actions/Socialstream/CreateConnectedAccount.php', app_path('Actions/Socialstream/CreateConnectedAccount.php'));
+        copy(__DIR__.'/../../stubs/app/Actions/Socialstream/GenerateRedirectForProvider.php', app_path('Actions/Socialstream/GenerateRedirectForProvider.php'));
+        copy(__DIR__.'/../../stubs/app/Actions/Socialstream/UpdateConnectedAccount.php', app_path('Actions/Socialstream/UpdateConnectedAccount.php'));
         copy(__DIR__.'/../../stubs/app/Actions/Socialstream/CreateUserFromProvider.php', app_path('Actions/Socialstream/CreateUserFromProvider.php'));
         copy(__DIR__.'/../../stubs/app/Actions/Socialstream/HandleInvalidState.php', app_path('Actions/Socialstream/HandleInvalidState.php'));
         copy(__DIR__.'/../../stubs/app/Actions/Socialstream/SetUserPassword.php', app_path('Actions/Socialstream/SetUserPassword.php'));
@@ -162,11 +189,17 @@ class InstallCommand extends Command
         copy(__DIR__.'/../../stubs/inertia/resources/js/Pages/Auth/Register.vue', resource_path('js/Pages/Auth/Register.vue'));
 
         // Profile views...
-        copy(__DIR__.'/../../stubs/inertia/resources/js/Pages/Profile/ConnectedAccountsForm.vue', resource_path('js/Pages/Profile/ConnectedAccountsForm.vue'));
-        copy(__DIR__.'/../../stubs/inertia/resources/js/Pages/Profile/SetPasswordForm.vue', resource_path('js/Pages/Profile/SetPasswordForm.vue'));
+        copy(__DIR__.'/../../stubs/inertia/resources/js/Pages/Profile/ConnectedAccountsForm.vue', resource_path('js/Pages/Profile/Partials/ConnectedAccountsForm.vue'));
+        copy(__DIR__.'/../../stubs/inertia/resources/js/Pages/Profile/SetPasswordForm.vue', resource_path('js/Pages/Profile/Partials/SetPasswordForm.vue'));
         copy(__DIR__.'/../../stubs/inertia/resources/js/Pages/Profile/Show.vue', resource_path('js/Pages/Profile/Show.vue'));
 
-        $this->replaceInFile("// 'github',", "'github'", config_path('socialstream.php'));
+        // Socialstream components
+        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/inertia/resources/js/Components/SocialstreamIcons', resource_path('js/Components/SocialstreamIcons'));
+        copy(__DIR__.'/../../stubs/inertia/resources/js/Components/ActionLink.vue', resource_path('js/Components/ActionLink.vue'));
+        copy(__DIR__.'/../../stubs/inertia/resources/js/Components/ConnectedAccount.vue', resource_path('js/Components/ConnectedAccount.vue'));
+        copy(__DIR__.'/../../stubs/inertia/resources/js/Components/Socialstream.vue', resource_path('js/Components/Socialstream.vue'));
+
+        $this->replaceInFile('// Providers::github(),', 'Providers::github(),', config_path('socialstream.php'));
     }
 
     /**
@@ -176,7 +209,10 @@ class InstallCommand extends Command
      */
     protected function ensureTeamsCompatibility()
     {
-        // User model...
+        // Service Provider...
+        copy(__DIR__.'/../../stubs/app/Providers/TeamsAuthServiceProvider.php', app_path('Providers/AuthServiceProvider.php'));
+
+        // User Model...
         copy(__DIR__.'/../../stubs/app/Models/UserWithTeams.php', app_path('Models/User.php'));
 
         // Jetstream Actions...
@@ -202,6 +238,70 @@ class InstallCommand extends Command
                 $appConfig
             ));
         }
+    }
+
+    /**
+     * Returns the path to the correct test stubs.
+     *
+     * @return string
+     */
+    protected function getTestStubsPath()
+    {
+        return $this->option('pest')
+            ? __DIR__.'/../../stubs/pest-tests'
+            : __DIR__.'/../../stubs/tests';
+    }
+
+    /**
+     * Install the given Composer Packages as "dev" dependencies.
+     *
+     * @param  mixed  $packages
+     * @return void
+     */
+    protected function requireComposerDevPackages($packages)
+    {
+        $composer = $this->option('composer');
+
+        if ($composer !== 'global') {
+            $command = [$this->phpBinary(), $composer, 'require', '--dev'];
+        }
+
+        $command = array_merge(
+            $command ?? ['composer', 'require', '--dev'],
+            is_array($packages) ? $packages : func_get_args()
+        );
+
+        (new Process($command, base_path(), ['COMPOSER_MEMORY_LIMIT' => '-1']))
+            ->setTimeout(null)
+            ->run(function ($type, $output) {
+                $this->output->write($output);
+            });
+    }
+
+    /**
+     * @return void
+     */
+    protected function installNodeDependenciesAndBuild()
+    {
+        $commands = ['npm install', 'npm run build'];
+
+        $this->runCommands($commands);
+    }
+
+    /**
+     * @param  array  $commands
+     * @param  array  $env
+     * @return Process
+     */
+    protected function runCommands(array $commands, array $env = [])
+    {
+        $process = Process::fromShellCommandline(implode(' && ', $commands), null, $env, null, null);
+
+        $process->run(function ($type, $line) {
+            $this->output->write('    '.$line);
+        });
+
+        return $process;
     }
 
     /**
