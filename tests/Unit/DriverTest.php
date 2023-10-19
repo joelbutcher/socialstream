1964c6702e021386f7536aef53acba162c38c535
Joel Butcher <joel.butcher@gosuperscript.com>
2023-10-19 17:30:30 +0100

    fix: DriverTest

--- tests/Unit/DriverTest.php
+++ tests/Unit/DriverTest.php
@@ -19,7 +19,7 @@ it('can build a composer require command for an array of packages', function () 
     $method = $reflection->getMethod('buildBaseComposerCommand');
     $method->setAccessible(true);
 
-    expect($method->invoke($driver, 'global', 'require', ['laravel/breeze']))
+    expect($method->invoke($driver, 'require', ['laravel/breeze']))
         ->toEqual([
             'composer', 'require', 'laravel/breeze',
         ]);
@@ -37,7 +37,7 @@ it('can build a composer require dev command for an array of packages', function
     $method = $reflection->getMethod('buildBaseComposerCommand');
     $method->setAccessible(true);
 
-    expect($method->invoke($driver, 'global', 'require', ['laravel/breeze'], dev: true))
+    expect($method->invoke($driver, 'require', ['laravel/breeze'], dev: true))
         ->toEqual([
             'composer', 'require', '--dev', 'laravel/breeze',
         ]);
@@ -55,7 +55,7 @@ it('can build a composer remove dev command for an array of packages', function 
     $method = $reflection->getMethod('buildBaseComposerCommand');
     $method->setAccessible(true);
 
-    expect($method->invoke($driver, 'global', 'remove', ['laravel/breeze'], dev: true))
+    expect($method->invoke($driver, 'remove', ['laravel/breeze'], dev: true))
         ->toEqual([
             'composer', 'remove', '--dev', 'laravel/breeze',
         ]);
