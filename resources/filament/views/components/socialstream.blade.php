4f7cfcff9d2bb2c97b52c19d84a0e618d1b3500f
Joel Butcher <joel.butcher@gosuperscript.com>
2023-10-23 18:56:28 +0100

    Update default dark mode styling

--- resources/filament/views/components/socialstream.blade.php
+++ resources/filament/views/components/socialstream.blade.php
@@ -2,9 +2,9 @@
     @if(! empty(\JoelButcher\Socialstream\Socialstream::providers()))
         <div class="relative flex items-center">
             <div class="flex-grow border-t border-gray-400 dark:border-gray-500"></div>
-            <span class="flex-shrink text-gray-400 dark:text-gray-500 px-6">
-            {{ config('socialstream.prompt', 'Or Login Via') }}
-        </span>
+            <span class="flex-shrink text-gray-400 dark:text-gray-300 px-6">
+                {{ config('socialstream.prompt', 'Or Login Via') }}
+            </span>
             <div class="flex-grow border-t border-gray-400 dark:border-gray-500"></div>
         </div>
     @endif
@@ -16,7 +16,7 @@
             <a class="flex gap-2 items-center justify-center transition duration-200 border border-gray-400 w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md"
                href='{{ route('oauth.redirect', $provider['id']) }}'>
                 <x-socialstream::socialstream-icons.provider-icon :provider="$provider['id']" class="h-6 w-6"/>
-                <span class="block font-medium text-sm text-gray-700 dark:text-gray-300">{{ $provider['buttonLabel'] }}</span>
+                <span class="block font-medium text-sm text-gray-700 dark:text-gray-200">{{ $provider['buttonLabel'] }}</span>
             </a>
         @endforeach
     </div>
