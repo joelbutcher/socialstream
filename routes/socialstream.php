5197a750bc53abdce41226e49a0a8e5b7e20bf5f
Joel Butcher <joelbutcher@users.noreply.github.com>
2023-11-06 23:08:12 +0000

    fix: duplicate route names

--- routes/socialstream.php
+++ routes/socialstream.php
@@ -5,6 +5,5 @@ use JoelButcher\Socialstream\Http\Controllers\OAuthController;
 
 Route::group(['middleware' => config('socialstream.middleware', ['web'])], function () {
     Route::get('/oauth/{provider}', [OAuthController::class, 'redirect'])->name('oauth.redirect');
-    Route::get('/oauth/{provider}/callback', [OAuthController::class, 'callback'])->name('oauth.callback');
-    Route::post('/oauth/{provider}/callback', [OAuthController::class, 'callback'])->name('oauth.callback');
+    Route::match(['get', 'post'], '/oauth/{provider}/callback', [OAuthController::class, 'callback'])->name('oauth.callback');
 });
