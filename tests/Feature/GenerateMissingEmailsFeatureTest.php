98dbb12848e1e914b88ac054b732d2bd4ee51b53
miguilim <35383529+srdante@users.noreply.github.com>
2023-11-30 18:28:12 -0300

    Split feature tests into different files

---
+++ tests/Feature/GenerateMissingEmailsFeatureTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace JoelButcher\Socialstream\Tests\Feature;
+
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Config;
+use JoelButcher\Socialstream\Features;
+use Laravel\Socialite\Facades\Socialite;
+use Laravel\Socialite\Two\GithubProvider;
+use Laravel\Socialite\Two\User as SocialiteUser;
+use Mockery;
+
+use function Pest\Laravel\get;
+
+uses(RefreshDatabase::class);
+
+it('generates missing emails', function (): void {
+    Config::set('socialstream.features', [
+        Features::generateMissingEmails(),
+    ]);
+
+    $user = (new SocialiteUser())
+        ->map([
+            'id' => $githubId = fake()->numerify('########'),
+            'nickname' => 'joel',
+            'name' => 'Joel',
+            'avatar' => null,
+            'avatar_original' => null,
+        ])
+        ->setToken('user-token')
+        ->setRefreshToken('refresh-token')
+        ->setExpiresIn(3600);
+
+    $provider = Mockery::mock(GithubProvider::class);
+    $provider->shouldReceive('user')->once()->andReturn($user);
+
+    Socialite::shouldReceive('driver')->once()->with('github')->andReturn($provider);
+
+    session()->put('socialstream.previous_url', route('register'));
+
+    get('http://localhost/oauth/github/callback')
+        ->assertRedirect(RouteServiceProvider::HOME);
+
+    $user = User::first();
+
+    $this->assertAuthenticated();
+    $this->assertEquals("$githubId@github", $user->email);
+    $this->assertDatabaseHas('connected_accounts', [
+        'provider' => 'github',
+        'provider_id' => $githubId,
+        'email' => $user->email,
+    ]);
+});
