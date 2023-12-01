ccfec506ebe5cf5e35cfb3ccda4108e47ee3b679
Miguilim <35383529+miguilimzero@users.noreply.github.com>
2023-12-01 01:01:59 -0300

    [5.x] Improve authentication & account registering logic (#318)
    * Improve alreadyRegistered error message
    
    * Improve registration logic
    
    * Split feature tests into different files
    
    * Add failure tests for createAccountOnFirstLogin feature
    
    * rename files
    
    ---------
    
    Co-authored-by: miguilim <35383529+srdante@users.noreply.github.com>
    Co-authored-by: Joel Butcher <joel.butcher@gosuperscript.com>

---
+++ tests/Feature/LoginOnRegistrationTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace JoelButcher\Socialstream\Tests\Feature;
+
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Hash;
+use JoelButcher\Socialstream\Features;
+use Laravel\Socialite\Facades\Socialite;
+use Laravel\Socialite\Two\GithubProvider;
+use Laravel\Socialite\Two\User as SocialiteUser;
+use Mockery;
+
+use function Pest\Laravel\get;
+
+uses(RefreshDatabase::class);
+
+test('users can login on registration', function (): void {
+    Config::set('socialstream.features', [
+        Features::loginOnRegistration(),
+    ]);
+
+    User::create([
+        'name' => 'Joel Butcher',
+        'email' => 'joel@socialstream.dev',
+        'password' => Hash::make('password'),
+    ]);
+
+    $this->assertDatabaseHas('users', ['email' => 'joel@socialstream.dev']);
+    $this->assertDatabaseEmpty('connected_accounts');
+
+    $user = (new SocialiteUser())
+        ->map([
+            'id' => $githubId = fake()->numerify('########'),
+            'nickname' => 'joel',
+            'name' => 'Joel',
+            'email' => 'joel@socialstream.dev',
+            'avatar' => null,
+            'avatar_original' => null,
+        ])
+        ->setToken('user-token')
+        ->setRefreshToken('refresh-token')
+        ->setExpiresIn(3600);
+
+    $provider = Mockery::mock(GithubProvider::class);
+    $provider->shouldReceive('user')->once()->andReturn($user);
+
+    Socialite::shouldReceive('driver')->once()->with('github')->andReturn($provider);
+
+    session()->put('socialstream.previous_url', route('register'));
+
+    get('http://localhost/oauth/github/callback')
+        ->assertRedirect(RouteServiceProvider::HOME);
+
+    $this->assertAuthenticated();
+    $this->assertDatabaseHas('connected_accounts', [
+        'provider' => 'github',
+        'provider_id' => $githubId,
+        'email' => 'joel@socialstream.dev',
+    ]);
+});
