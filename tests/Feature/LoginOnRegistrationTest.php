6373cc0d7a7a6f5c7fe2ae37dc869d68aba699f9
miguilim <35383529+srdante@users.noreply.github.com>
2024-01-27 02:45:51 -0300

    Add cannot test with no loginOnRegistration enabled

--- tests/Feature/LoginOnRegistrationTest.php
+++ tests/Feature/LoginOnRegistrationTest.php
@@ -61,3 +61,81 @@ test('users can login on registration', function (): void {
         'email' => 'joel@socialstream.dev',
     ]);
 });
+
+test('users cannot login on registration without feature enabled', function (): void {
+    User::create([
+        'name' => 'Joel Butcher',
+        'email' => 'joel@socialstream.dev',
+        'password' => Hash::make('password'),
+    ]);
+
+    $this->assertDatabaseHas('users', ['email' => 'joel@socialstream.dev']);
+    $this->assertDatabaseEmpty('connected_accounts');
+
+    $user = (new SocialiteUser())
+        ->map([
+            'id' => $githubId = fake()->numerify('########'),
+            'nickname' => 'joel',
+            'name' => 'Joel',
+            'email' => 'joel@socialstream.dev',
+            'avatar' => null,
+            'avatar_original' => null,
+        ])
+        ->setToken('user-token')
+        ->setRefreshToken('refresh-token')
+        ->setExpiresIn(3600);
+
+    $provider = Mockery::mock(GithubProvider::class);
+    $provider->shouldReceive('user')->once()->andReturn($user);
+
+    Socialite::shouldReceive('driver')->once()->with('github')->andReturn($provider);
+
+    session()->put('socialstream.previous_url', route('register'));
+
+    $response = get('http://localhost/oauth/github/callback');
+
+    $this->assertGuest();
+    $response->assertRedirect(route('register'));
+    $response->assertSessionHasErrors();
+});
+
+test('users cannot login on registration from random route without feature enabled (but globalLogin + createAccountOnFirstLogin)', function (): void {
+    Config::set('socialstream.features', [
+        Features::globalLogin(),
+        Features::createAccountOnFirstLogin(),
+    ]);
+
+    User::create([
+        'name' => 'Joel Butcher',
+        'email' => 'joel@socialstream.dev',
+        'password' => Hash::make('password'),
+    ]);
+
+    $this->assertDatabaseHas('users', ['email' => 'joel@socialstream.dev']);
+    $this->assertDatabaseEmpty('connected_accounts');
+
+    $user = (new SocialiteUser())
+        ->map([
+            'id' => $githubId = fake()->numerify('########'),
+            'nickname' => 'joel',
+            'name' => 'Joel',
+            'email' => 'joel@socialstream.dev',
+            'avatar' => null,
+            'avatar_original' => null,
+        ])
+        ->setToken('user-token')
+        ->setRefreshToken('refresh-token')
+        ->setExpiresIn(3600);
+
+    $provider = Mockery::mock(GithubProvider::class);
+    $provider->shouldReceive('user')->once()->andReturn($user);
+
+    Socialite::shouldReceive('driver')->once()->with('github')->andReturn($provider);
+
+    session()->put('socialstream.previous_url', '/random');
+
+    $response = get('http://localhost/oauth/github/callback');
+
+    $this->assertGuest();
+    $response->assertSessionHasErrors();
+});
