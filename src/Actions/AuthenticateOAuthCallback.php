f3b6e366150ab33af839cb839cdc1e0ea793cd60
Joel Butcher <joelbutcher@users.noreply.github.com>
2024-07-18 13:05:33 +0100

    fix: checking filament user and admin routing

--- src/Actions/AuthenticateOAuthCallback.php
+++ src/Actions/AuthenticateOAuthCallback.php
@@ -207,11 +207,18 @@ class AuthenticateOAuthCallback implements AuthenticatesOAuthCallback
             return false;
         }
 
+        if ($this->hasComposerPackage('filament/filament')) {
+            return (Route::has('filament.auth.login') && Session::get('socialstream.previous_url') === route('filament.auth.login')) ||
+                (Route::has('filament.admin.auth.login') && Session::get('socialstream.previous_url') === route('filament.admin.auth.login')) ||
+                (Route::has('filament.auth.register') && Session::get('socialstream.previous_url') === route('filament.auth.register')) ||
+                (Route::has('filament.admin.auth.register') && Session::get('socialstream.previous_url') === route('filament.admin.auth.register'));
+        }
+
         if (Route::has('register') && Session::get('socialstream.previous_url') === route('register')) {
             return true;
         }
 
-        if (Session::get('socialstream.previous_url') !== route('login')) {
+        if (Route::has('login') && Session::get('socialstream.previous_url') !== route('login')) {
             return Features::hasGlobalLoginFeatures();
         }
 
