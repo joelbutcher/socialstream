36bf3aa34cfc5e18ccc993493c60f1f243d30f6f
Joel Butcher <joelbutcher@users.noreply.github.com>
2024-09-19 14:57:13 +0100

    [6.x] fix removed auth props in Laravel Breeze support (#371)

--- stubs/breeze/inertia-react-ts/resources/js/Pages/Profile/Edit.tsx
+++ stubs/breeze/inertia-react-ts/resources/js/Pages/Profile/Edit.tsx
@@ -4,16 +4,15 @@ import DeleteUserForm from './Partials/DeleteUserForm';
 import SetPasswordForm from './Partials/SetPasswordForm';
 import UpdatePasswordForm from './Partials/UpdatePasswordForm';
 import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm';
-import { Head } from '@inertiajs/react';
-import { PageProps, Socialstream } from '@/types';
+import {Head} from '@inertiajs/react';
+import {PageProps, Socialstream} from '@/types';
 
-export default function Edit({ auth, mustVerifyEmail, status, socialstream }: PageProps<{ mustVerifyEmail: boolean, status?: string, socialstream: Socialstream}>) {
+export default function Edit({mustVerifyEmail, status, socialstream}: PageProps<{ mustVerifyEmail: boolean, status?: string, socialstream: Socialstream }>) {
     return (
         <AuthenticatedLayout
-            user={auth.user}
             header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Profile</h2>}
         >
-            <Head title="Profile" />
+            <Head title="Profile"/>
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
@@ -26,18 +25,26 @@ export default function Edit({ auth, mustVerifyEmail, status, socialstream }: Pa
                     </div>
 
                     <div className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                        {socialstream.hasPassword ? <UpdatePasswordForm className="max-w-xl" /> : <SetPasswordForm className="max-w-xl" />}
+                        {socialstream.hasPassword ? (
+                            <UpdatePasswordForm className="max-w-xl"/>
+                        ) : (
+                            <SetPasswordForm className="max-w-xl"/>
+                        )}
                     </div>
 
                     {socialstream.show && (
                         <div className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                            <ConnectedAccountsForm providers={socialstream.providers} connectedAccounts={socialstream.connectedAccounts} hasPassword={socialstream.hasPassword} />
+                            <ConnectedAccountsForm
+                                providers={socialstream.providers}
+                                connectedAccounts={socialstream.connectedAccounts}
+                                hasPassword={socialstream.hasPassword}
+                            />
                         </div>
                     )}
 
                     {socialstream.hasPassword && (
                         <div className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                            <DeleteUserForm className="max-w-xl" />
+                            <DeleteUserForm className="max-w-xl"/>
                         </div>
                     )}
                 </div>
