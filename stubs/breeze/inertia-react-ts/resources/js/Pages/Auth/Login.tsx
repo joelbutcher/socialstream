b27acb7ab207248a20203f210f8cc0a7f13d41d4
Joel Butcher <joelbutcher@users.noreply.github.com>
2025-01-17 19:04:39 +0000

    fix login.tsx

--- stubs/breeze/inertia-react-ts/resources/js/Pages/Auth/Login.tsx
+++ stubs/breeze/inertia-react-ts/resources/js/Pages/Auth/Login.tsx
@@ -1,38 +1,50 @@
-import { useEffect, FormEventHandler } from 'react';
 import Checkbox from '@/Components/Checkbox';
-import GuestLayout from '@/Layouts/GuestLayout';
 import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
 import Socialstream from '@/Components/Socialstream';
+import TextInput from '@/Components/TextInput';
+import GuestLayout from '@/Layouts/GuestLayout';
 import { Head, Link, useForm } from '@inertiajs/react';
+import { FormEventHandler } from 'react';
 import { Socialstream as SocialstreamType } from '@/types';
 
-export default function Login({ status, canResetPassword, socialstream, errors: { socialstream: socialstreamErrors} }: { status?: string, canResetPassword: boolean, socialstream: SocialstreamType, errors: { socialstream?: string } }) {
+export default function Login({
+    status,
+    canResetPassword,
+    socialstream,
+    errors: {
+        socialstream: socialstreamErrors
+    }
+}: {
+    status?: string;
+    canResetPassword: boolean;
+    socialstream: SocialstreamType;
+    errors: { socialstream?: string };
+}) {
     const { data, setData, post, processing, errors, reset } = useForm({
         email: '',
         password: '',
         remember: false,
     });
 
-    useEffect(() => {
-        return () => {
-            reset('password');
-        };
-    }, []);
-
     const submit: FormEventHandler = (e) => {
         e.preventDefault();
 
-        post(route('login'));
+        post(route('login'), {
+            onFinish: () => reset('password'),
+        });
     };
 
     return (
         <GuestLayout>
             <Head title="Log in" />
 
-            {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
+            {status && (
+                <div className="mb-4 text-sm font-medium text-green-600">
+                    {status}
+                </div>
+            )}
 
             <form onSubmit={submit}>
                 <div>
@@ -68,22 +80,26 @@ export default function Login({ status, canResetPassword, socialstream, errors: 
                     <InputError message={errors.password} className="mt-2" />
                 </div>
 
-                <div className="block mt-4">
+                <div className="mt-4 block">
                     <label className="flex items-center">
                         <Checkbox
                             name="remember"
                             checked={data.remember}
-                            onChange={(e) => setData('remember', e.target.checked)}
+                            onChange={(e) =>
+                                setData('remember', e.target.checked)
+                            }
                         />
-                        <span className="ms-2 text-sm text-gray-600 dark:text-gray-400">Remember me</span>
+                        <span className="ms-2 text-sm text-gray-600 dark:text-gray-400">
+                            Remember me
+                        </span>
                     </label>
                 </div>
 
-                <div className="flex items-center justify-end mt-4">
+                <div className="mt-4 flex items-center justify-end">
                     {canResetPassword && (
                         <Link
                             href={route('password.request')}
-                            className="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                            className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800"
                         >
                             Forgot your password?
                         </Link>
